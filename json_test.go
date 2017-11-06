a7e0ab905347f9b77ac1fbc54e08b7094c7cb5c8
scheshan <5373827@qq.com>
2017-11-06 23:30:45 +0800

    Add unit test

--- json_test.go
+++ json_test.go
@@ -5,21 +5,29 @@ import "testing"
 import "strings"
 
 func Test_GetDataFromReader(t *testing.T) {
-	str := `{"id":1,"name":"aaa","data":{"id":1}}`
+	str := `{"key1":1,"key2":"aaa","key3":true,"key4":{"key1":1},"key5":[1,2,3]}`
 	reader := strings.NewReader(str)
 
 	s := &jsonSource{}
 	data := s.getDataFromReader(reader)
 
-	if data["id"] != "1" {
-		t.Error("Get id error")
+	if data["key1"] != "1" {
+		t.Error("Get key1 error")
 	}
 
-	if data["name"] != "aaa" {
-		t.Error("Get name error")
+	if data["key2"] != "aaa" {
+		t.Error("Get key2 error")
 	}
 
-	if data["data:id"] != "1" {
-		t.Error("Get data:id error")
+	if data["key3"] != "true" {
+		t.Error("Get key3 error")
+	}
+
+	if data["key4:key1"] != "1" {
+		t.Error("Get key4:key1 error")
+	}
+
+	if data["key5"] != "[1 2 3]" {
+		t.Error("Get key5 error")
 	}
 }
