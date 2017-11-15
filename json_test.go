ba2e38cb8dfa7e86e0331cd94de16be80a22857b
scheshan <5373827@qq.com>
2017-11-15 22:03:19 +0800

    Add unit test

--- json_test.go
+++ json_test.go
@@ -1,8 +1,12 @@
 package gonfig
 
-import "testing"
-
-import "strings"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
 
 func Test_GetDataFromReader(t *testing.T) {
 	str := `{"key1":1,"key2":"aaa","key3":true,"key4":{"key1":1},"key5":[1,2,3]}`
@@ -29,5 +33,65 @@ func Test_GetDataFromReader(t *testing.T) {
 
 	if data["key5"] != "[1 2 3]" {
 		t.Error("Get key5 error")
+	}
+}
+
+func Test_AddJSON(t *testing.T) {
+	path := randomString(20)
+
+	builder := NewBuilder()
+	AddJSON(builder, path)
+
+	pList := builder.GetSources()
+	if len(pList) != 1 {
+		t.Error("Provider length error")
+	}
+	p, ok := pList[0].(*jsonSource)
+	if !ok {
+		t.Error("Type cast error")
+	}
+	if p.path != path {
+		t.Error("Path error")
+	}
+}
+
+func Test_JSONSetCallbackChannel(t *testing.T) {
+	s := &jsonSource{}
+	ch := make(chan IConfigSource)
+	s.SetCallbackChannel(ch)
+
+	if s.ch != ch {
+		t.Error("Set callback channel error")
+	}
+}
+
+func Test_JSONGetData(t *testing.T) {
+	p := filepath.Join(os.TempDir(), randomString(20))
+
+	fmt.Println(p)
+
+	defer os.Remove(p)
+
+	file, err := os.Create(p)
+	if err != nil {
+		t.Error(err)
+	}
+
+	content := `{"Key1":"Value1"}`
+
+	file.WriteString(content)
+	file.Close()
+
+	c := &jsonSource{
+		path: p,
+	}
+	data := c.GetData()
+
+	v, ok := data["Key1"]
+	if !ok {
+		t.Error("Get Key1 error")
+	}
+	if v != "Value1" {
+		t.Error("Get Key1 error")
 	}
 }
